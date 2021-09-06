3528233eae3d667326506730e7c654ef83f9a5f1
gturner7 <66582166+gturner7@users.noreply.github.com>
2021-09-06 18:10:05 -0400

    Update Rcleaning.R

--- Code/R/Rcleaning.R
+++ Code/R/Rcleaning.R
@@ -1,3 +1,5 @@
+library(DescTools)
+
 #import datasets
 data<-read.csv('pppub20.csv')
 hh<-read.csv('hhpub20.csv')
@@ -19,7 +21,21 @@ min(data$H_IDNUM)
 id_table<-table(data$PERIDNUM)
 
 #merge datasets
-new<-merge(data,hh_subset,by='H_IDNUM')
+data_with_outliers<-merge(data,hh_subset,by='H_IDNUM')
 
 #export new combined data
-write.csv(new, file='Hydrogen_Data.csv')
+write.csv(data_with_outliers, file='2020_Data_With_Outliers.csv')
+
+#remove outliers
+quantiles<-data.frame(quantile(data_with_outliers$PEARNVAL))
+iqr<-quantiles['75%',]-quantiles['25%',]
+maxy<-quantiles['75%',]+iqr*1.5
+miny<-quantiles['25%',]-iqr*1.5
+#miny has no effect
+
+data_without_outliers<-subset(data_with_outliers, PEARNVAL>= miny & PEARNVAL<=maxy)
+summary(data_without_outliers$PEARNVAL)
+#summary shows values from -9,999:100,000
+
+#export data w/out outliers
+write.csv(data_without_outliers, file='2020_Data_Without_Outliers.csv')
